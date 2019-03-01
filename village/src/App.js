83655cae9970569c0c3836797e3e20c02adca903
Maxime Salomon <maxime@croissant.io>
2019-03-01 10:08:49 +0100

    added Routes

--- village/src/App.js
+++ village/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Route, Link } from "react-router-dom"; 
+
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
@@ -39,8 +41,24 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <SmurfForm />
-        <Smurfs smurfs={this.state.smurfs} />
+      <Route
+        exact path="/"
+        render={props =>
+          <Smurfs
+            {...props} 
+            smurfs={this.state.smurfs} />
+        }
+      />
+
+      <Route
+        path="/smurf-form"
+        render={ props =>
+          <SmurfForm
+            {...props}
+            smurf={this.state.smurf}
+          />
+        }
+      />
       </div>
     );
   }
