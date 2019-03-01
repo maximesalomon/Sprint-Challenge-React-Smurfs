4f3670fa223a966213a2f1b931137c3713319b41
Maxime Salomon <maxime@croissant.io>
2019-03-01 10:39:45 +0100

    MVP

--- village/src/components/Smurf.js
+++ village/src/components/Smurf.js
@@ -1,12 +1,30 @@
 import React from 'react';
+import styled from 'styled-components'
+
+const SmurfStyle = styled.div `
+  display: flex;
+`
+const SmurfName = styled.div `
+  padding: 10px;
+  margin-bottom: 7px;
+  width: 200px;
+  border: 1px solid blue;
+`
+
+const SmurfItem = styled.div `
+  padding: 10px;
+  margin-bottom: 7px;
+  width: 140px;
+  border: 1px solid blue;
+`
 
 const Smurf = props => {
   return (
-    <div className="Smurf">
-      <h3>{props.name}</h3>
-      <strong>{props.height} tall</strong>
-      <p>{props.age} smurf years old</p>
-    </div>
+    <SmurfStyle>
+      <SmurfName>Name: {props.name}</SmurfName>
+      <SmurfItem>Height: {props.height}</SmurfItem>
+      <SmurfItem>Age: {props.age}</SmurfItem>
+    </SmurfStyle>
   );
 };
 
